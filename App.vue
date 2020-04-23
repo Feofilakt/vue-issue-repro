3a76118dba603b5594c65268594b9e7145fb66c5
Vyacheslav Chernykh <Feofilakt@users.noreply.github.com>
2020-04-23 18:23:43 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<input type="checkbox" id="checkbox" v-model="enabled" />
+		<label for="checkbox">{{ enabled ? "turn off" : "turn on" }}</label>
+
+		<child v-if="enabled" />
+	</div>
+</template>
+
+<script>
+	import Child from "./Child.vue";
+
+	export default {
+		name: "App",
+		components: { Child },
+		data: function() {
+			return {
+				enabled: false
+			};
+		}
+	};
+</script>
---
+++ Child.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>{{ a.length }}</div>
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {
+				a: []
+			};
+		},
+		created: function() {
+			for (var i = 0; i < 50000; i++) {
+				this.a.push({
+					b: 1,
+					c: 2,
+					d: 3,
+					e: 4
+				});
+			}
+		}
+	};
+</script>
